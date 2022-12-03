e01c5e4c1b8dd27152591e93deae13430b531377
harsha6968 <goneharshavardhan96962@gmail>
2022-12-03 16:48:45 +0530

    hi

--- nov-29/pra8.js
+++
@@ -1,7 +0,0 @@
-function add(a,b){
-    return a+b;
-}
-let num1="parseInt"("enter a number:"+a)
-let num2="parseInt"("enter a number:"+b)
-let result=add(num1,num2)
-console.log("the sum is"+result)
